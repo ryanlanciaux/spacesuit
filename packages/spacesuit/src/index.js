c95d6f0ea82d9a9014835683a6a21db5898922a8
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2019-08-21 11:14:04 -0400

    Update includes

--- packages/spacesuit/src/index.js
+++ packages/spacesuit/src/index.js
@@ -2,4 +2,6 @@ export { default as Button } from "@spacesuit/button";
 export { default as Box } from "@spacesuit/box";
 export { default as Flex } from "@spacesuit/flex";
 export { default as Heading } from "@spacesuit/heading";
-export { default as Link } from "@spacesuiit/link";
+export { default as Header } from "@spacesuit/header";
+export { default as Link } from "@spacesuit/link";
+export { ThemeProvider, theme } from "@spacesuit/theming";
