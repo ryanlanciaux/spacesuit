29db30dda8518c6e00b672cc3bbf4fb6b0f34dea
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2019-11-29 15:26:50 -0500

    Update header to make the header background more consistent

--- packages/header/src/index.js
+++ packages/header/src/index.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Box from "@spacesuit/box";
+import styled from "@emotion/styled";
 import { NavList, NavListItem } from "@spacesuit/navlist";
 
-function Header({ backgroundUrl, ...props }) {
-  return <Box {...props} backgroundUrl={backgroundUrl} as="header" />;
+const BackgroundBox = styled(Box)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+const Content = styled(Box)`
+  position: relative;
+`;
+
+function Header({ backgroundUrl, bg, ...props }) {
+  return (
+    <Box {...props} as="header" style={{ position: "relative" }}>
+      <BackgroundBox backgroundUrl={backgroundUrl} />
+      <BackgroundBox bg={bg} />
+      <Content>{props.children}</Content>
+    </Box>
+  );
 }
 
 export default Header;
