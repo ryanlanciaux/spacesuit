b7ecb57fb33700fe76e9a1225980396273738ef3
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2019-11-29 15:54:53 -0500

    Update footer for background / background image

--- packages/footer/src/index.js
+++ packages/footer/src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import Box from "@spacesuit/box";
+import styled from "@emotion/styled";
+import { NavList, NavListItem } from "@spacesuit/navlist";
 
-export default function footer({ children, ...props }) {
+const BackgroundBox = styled(Box)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+const Content = styled(Box)`
+  position: relative;
+`;
+
+export default function footer({ backgroundUrl, bg, ...props }) {
   return (
-    <Box as="footer" {...props}>
-      {children}
+    <Box {...props} as="footer" style={{ position: "relative" }}>
+      <BackgroundBox backgroundUrl={backgroundUrl} />
+      <BackgroundBox bg={bg} />
+      <Content>{props.children}</Content>
     </Box>
   );
 }
--- packages/stories/stories/Footer/Footer.stories.js
+++ packages/stories/stories/Footer/Footer.stories.js
@@ -5,5 +5,22 @@ import { storiesOf } from "@storybook/react";
 import { Footer } from "@spacesuit/spacesuit";
 
 storiesOf("Footer", module).add("default", () => {
-  return <Footer bg="primary" color="gray" p={4} />;
+  return (
+    <Footer
+      bg="rgba(65, 152, 132, .4)"
+      color="gray"
+      p={4}
+      backgroundUrl="https://source.unsplash.com/random/800x600"
+    >
+      <p>
+        Quis aliquip Lorem aute id incididunt culpa velit do tempor consequat
+        elit exercitation excepteur. Id qui duis elit incididunt nulla ex sit
+        anim. Sunt et laborum Lorem laboris magna commodo dolore. Nisi cupidatat
+        velit nisi amet Lorem aute esse sunt adipisicing excepteur nostrud.
+        Laboris commodo excepteur laboris ipsum culpa cupidatat sit veniam
+        ullamco nostrud fugiat ullamco. Exercitation dolor ullamco fugiat magna
+        aute.
+      </p>
+    </Footer>
+  );
 });
